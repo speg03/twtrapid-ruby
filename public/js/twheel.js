720d2a597e6b60012df654a49b3424a883fb1c46
speg03 <speg03+github@gmail.com>
2009-10-11 22:03:21 +0900

    ソースの整形。

--- public/js/twheel.js
+++ public/js/twheel.js
@@ -97,9 +97,18 @@ function format_status(status) {
     var status_line = $('<div class="status">');
     status_line.attr('id', status.id);
 
-    $('<img class="icon">').attr('src', status.user.profile_image_url).attr('alt', status.user.screen_name).appendTo(status_line);
-    $('<span class="name">').text(status.user.screen_name).appendTo(status_line);
-    $('<span class="text">').html(': ' + status.text).appendTo(status_line);
+    $('<img class="icon">')
+        .attr('src', status.user.profile_image_url)
+        .attr('alt', status.user.screen_name)
+        .appendTo(status_line);
+
+    $('<span class="name">')
+        .text(status.user.screen_name)
+        .appendTo(status_line);
+
+    $('<span class="text">')
+        .html(': ' + status.text)
+        .appendTo(status_line);
 
     return status_line;
 }
